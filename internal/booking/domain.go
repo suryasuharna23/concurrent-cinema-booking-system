68ec69575899ab6989529ede7e9a6ba2537f3fc9
Surya Suharna <19623125@std.stei.itb.ac.id>
2026-03-24 14:09:57 +0700

    feat: add data struct and memory store interface

--- internal/booking/domain.go
+++ internal/booking/domain.go
@@ -1,1 +1,20 @@
 package booking
+
+import "errors"
+
+var (
+	ErrSeatAlreadyBooked = errors.New("seat is already taken")
+)
+
+type Booking struct {
+	ID      string
+	MovieID string
+	SeatID  string
+	UserID  string
+	Status  string
+}
+
+type BookingStore interface {
+	Book(b Booking) error
+	ListBookings(movieID string) []Booking
+}
--- internal/booking/memory_store.go
+++ internal/booking/memory_store.go
@@ -1,1 +1,27 @@
 package booking
+
+type MemoryStore struct {
+	bookings map[string]Booking
+}
+
+func NewMemoryStore() *MemoryStore{
+	return &MemoryStore{
+		bookings: map[string]Booking{},
+	}
+}
+
+func (*s MemoryStore) Book(b Booking) error{
+	if _, exist := s.bookings[b.SeatID]; exist {
+		return ErrSeatAlreadyBooked
+	}
+}
+
+func (*s MemoryStore) ListBookings(movieID string) []Booking {
+	var result []Booking
+	for _, b:= range s.bookings{
+		if b.movieID == movieID{
+			result = append(result, b)
+		} 
+	}
+	return result
+}
